a80c748ee845d590df5d99a9ea1858eccb04cf5b
Rahul Kumar Sahu <rahulsahu.cse@gmail.com>
2025-12-17 03:41:25 +0530

    refactoring sessionController.js file

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -96,9 +96,18 @@ export async function joinSession(req, res) {
             return res.status(404).json({message: "Session not found"});
         }
 
+        if (session.status !== "active") {
+            return res.status(400).json({message: "Cannot join a completed session"});
+        }
+
+        if (session.host.toString() === userId.toString()) {
+            return res.status(400).json({message: "Host cannot join their own session as participant"});
+        }
+
+
         // check if session is already full - has a participant
         if (session.participants) {
-            return res.status(400).json({message: "Session is already full"});
+            return res.status(409).json({message: "Session is already full"});
         }
 
         session.participants = userId;
@@ -135,8 +144,6 @@ export async function endSession(req, res) {
             return res.status(400).json({message: "Session is already completed"});
         }
 
-        session.status = "completed";
-        await session.save();
 
         //detete stream video call
         const call = streamClient.video.call("default", session.callId);
@@ -145,6 +152,10 @@ export async function endSession(req, res) {
         //delete stream chat channel
         const channel = chatClient.channel("messaging", session.callId);
         await channel.delete();
+
+        
+        session.status = "completed";
+        await session.save();
 
         res.status(200).json({message: "Session ended successfully" });
     } catch (error) {
